c02d7b848f5d071ffa16dc73a90f2a1c3c1a699c
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-03-23 11:15:35 -0700

    Created the initial elements of the game and added some javascript code for the basic mechanics.

---
+++ javascript-project-test/script.js
@@ -0,0 +1,20 @@
+const cookie = document.getElementById('fakeCookie');
+const cookieAmountDisplay = document.getElementById('cookieAmount');
+const cookieTotalDisplay = document.getElementById('cookieTotal')
+let cookieAmount = 0;
+let cookieTotal = 0;
+let addCookie = 1;
+
+function cookieClicked() {
+    cookieAmount += addCookie;
+    cookieTotal += 1;
+    
+    cookieAmountDisplay.innerHTML = `<h4>Cookie Crumbs Collected: ${cookieAmount}</h4>`;
+    cookieTotalDisplay.innerHTML = `<h4>Total Cookie Crumbs Clicked: ${cookieTotal}</h4>`;
+};
+
+cookie.addEventListener('click', cookieClicked);
+cookie.addEventListener('mousedown', () => {cookie.style.color = 'red';});
+cookie.addEventListener('mouseup', () => {cookie.style.color = 'black';});
+document.querySelector('body').addEventListener('keydown', (e) => {e.key === 's' ? addCookie = 2 : addCookie = 1});
+document.querySelector('body').addEventListener('keyup', () => {addCookie = 1});
