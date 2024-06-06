96b6186dcbf48eb86156564b33b700ebca3d0f77
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-06-06 06:09:21 -0700

    Found another solution for the two sum problem from leet code

--- AlgoExpert-LeetCode/two-sum/app.js
+++ AlgoExpert-LeetCode/two-sum/app.js
@@ -28,3 +28,44 @@ function twoSumV2(nums, target) {
         }
     }
 }
+
+// I ended up switching to the while loop and it is actually slower again. It seems that the indexOf use is causing this delay so I'm going to try and find a way around it.
+function twoSumsV3(nums, target) {
+    let num = 0;
+    let found = false;
+    let result = [];
+
+    while (found === false) {
+        let currentNum = nums.lastIndexOf(target - nums[num])
+        if (currentNum !== -1 && currentNum !== num) {
+            result = [num, currentNum];
+            found = true;
+        }
+        num++;
+    }
+
+    return result;
+};
+
+// After looking at some of the discussion, people recommended doing it with a hashmap. While I initially attempted this, I ran into the problem since I was using arrays, I had to either shallow
+// copy the array and then use indexOf to find the potential value which was causing a lot of additional time to execute. The couple of comments I found recommended using a object instead,
+// this allows me to not only store the seen value but to also include where its index is in the array.
+function twoSumsV4(nums, target) {
+    let seen = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        let potential = target - nums[i];
+        if (seen.hasOwnProperty(potential) && seen[potential] !== i) {
+            return [i, seen[potential]];
+        } else {
+            seen[nums[i]] = i;
+        }
+    }
+};
+
+
+// Biggest takeaway, just because something works does not mean it can't become more optimized. Even milliseconds add up and can result in a lot of time being spent when all one has to do is
+// spend a bit more time rewriting or finding a better solution. While it may seem like a waste of time, it almost never is since finding a way to save time will add up greatly in the long run.
+// There is a point of diminishing return so I shouldn't become too obsessive with it, like my first solution was only around 80ms to execute while this newest one was around 40ms. It is double the 
+// time but it still works if needed. However, in the case of my other solutions that used around 250ms of time, that is by far a much greater increase and makes the time spent finding a 
+// better solution far more worth it.
