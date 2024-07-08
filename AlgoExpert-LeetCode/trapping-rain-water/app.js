d9c7aac6d688327915a672185ab508202e6ef856
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-07-08 07:17:02 -0700

    Finished creating the webpages for all of the leetcode problems I have solved up to this point.

--- AlgoExpert-LeetCode/trapping-rain-water/app.js
+++ AlgoExpert-LeetCode/trapping-rain-water/app.js
@@ -58,3 +58,44 @@ function trappingRainWaterV1(height) {
 
     return trappedWater;
 };
+
+// Biggest takeaway, by far, my biggest takeaway is seeing how much I struggled to deal with pointers and my reliance on for loops. While for loops are not inherently bad and even nested for loops have their uses, they are not
+// the method I should try and adopt right away without fully understanding what I am trying to accomplish. Also, when it comes to leet code or algorithm questions like this, I need to start looking more into finding the formula or
+// thinking about the solution more as my lack of doing so is causing me to try and brute force the solution. In doing it this way, I am creating solutions that don't work once a lot of data is run through them or that take forever.
+// If I start to truly try and understand what the issue or problem I am trying to solve needs in order to be satisfied in all cases, then I should be able to start finding patterns or know to search up formulas that can make solving
+// the problem far easier and more straight forward.
+
+// Initializes the html elements from the webpage.
+const initialDisplay = document.getElementById('initial-display');
+const resultDisplay = document.getElementById('result-display');
+const radioOptions = document.getElementById('radio-options').querySelectorAll('input');
+
+// Displays the results from the algorithm.
+function displayResult(num) {
+    // Initializes a variable to store an array
+    let containerArray = [];
+    // Checks the value of the number passed in and stores its associated array in the containerArray variable
+    if (num == 0) {
+        containerArray = [0,1,0,2,1,0,1,3,2,1,2,1];
+    } else if (num == 1) {
+        containerArray = [4,2,0,3,2,5];
+    }
+
+    // Displays the initial array in the initial display element.
+    initialDisplay.innerHTML = JSON.stringify(containerArray);
+
+    // Displays the algorithm's result in the result display element.
+    resultDisplay.innerHTML = trappingRainWaterV1(containerArray);
+}
+// Loads the default eventListeners for the webpage.
+function loadEventListeners() {
+    // Iterates through the radio elements on the webpage.
+    for (const radio of radioOptions) {
+        // Initializes an evenListener for each radio element that calls the displayResult function.
+        radio.addEventListener('change', () => {
+            displayResult(radio.value)
+        })
+    }
+}
+
+loadEventListeners();
