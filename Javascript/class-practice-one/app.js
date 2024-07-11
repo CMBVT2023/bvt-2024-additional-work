35a26a56279d9f56a9e80fdc5a42eb74f4992f77
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-07-10 19:42:00 -0700

    Added a in class webpage project for practice javascript

---
+++ Javascript/class-practice-one/app.js
@@ -0,0 +1,34 @@
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+
+
+let exampleObject = {
+    id: 1,
+    name: "Cristiano"
+}
+
+
+console.log(exampleObject);
+console.log("My name is: ", exampleObject.name);
+
+
+function doSomething(){
+    let functionVar = 10;
+    for(let i = 15; i >= 0; i--){
+        console.log(i);
+    }
+}
+
+function sayMyName() {
+    console.log("Hello", exampleObject.name);
+}
+
+function logVar() {
+    console.log(myVar)
+}
+
+doSomething();
