60bb61a4b949beafd483a775c2a5f24d1a20b799
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-07-22 19:19:35 -0700

    Left off on a new class example for creating a palidrome checker.

---
+++ Javascript/class-practice/js/palindrome-checker.js
@@ -0,0 +1,19 @@
+// Define a function that takes in a string and returns a boolean, true or false, indicating whether it is a palindrome.
+
+// Define a function that takes in a string
+// Index through each character (including dashes and spaces)
+// Flip the string and index through comparing the words
+// Use if statements to make the comparisons, if the letters match continue comparing, if they don't return false.
+// If the index reaches the end of the word and all letters match, return true.
+
+function palindromeChecker(word) {
+    let arr = [];
+    arr = word.split('')
+
+    // Left off on trying to reverse the array and store it in a new variable.
+    
+    // return false;
+}
+
+let result = palindromeChecker("racecar")
+console.log(result)
