94acd22b531a11e415c3439cd5ebba838d9cff92
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-08-04 09:01:06 -0700

    Finished Video #7 of the react tutorial.

--- React/react-tutorial-follow-along/src/components/Home.jsx
+++ React/react-tutorial-follow-along/src/components/Home.jsx
@@ -1,7 +1,22 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('Hello World', e)
+    };
+
+    const handleClickAgain = (event, string) => {
+        console.log('Hello World, and ' + string + '.')
+        console.log(event.target)
+    };
+ 
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            {/* When invoking a function with no parameters */}
+            <button onClick={handleClick}>Click Me</button>
+
+            {/* When invoking a function with parameters */}
+            <button onClick={(e) => handleClickAgain(e, 'Yoshi')}>Click Me Again</button>
         </div>
      );
 }
