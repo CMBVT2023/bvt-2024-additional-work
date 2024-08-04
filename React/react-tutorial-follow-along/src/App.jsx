e893508ca571d4faf8cc79b58f7d1dc2493427e9
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-08-04 08:33:01 -0700

    Finished Video #1-4 of the react tutorial so far.

--- React/react-tutorial-follow-along/src/App.jsx
+++ React/react-tutorial-follow-along/src/App.jsx
@@ -1,18 +1,25 @@
 import React, { useState, useEffect } from 'react';
 
-// Left Off On Video #3
-
 function App() {
-  
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  // const person = {name: 'yoshi', age: 30}
+  const link = 'https://www.google.com'
+
   return (
-    <div className="div">
-      <ul>
-        <li>Test 1</li>
-        <li>Test 1</li>
-        <li>Test 1</li>
-        <li>Test 1</li>
-        <li>Test 1</li>
-      </ul>
+    <div className="App">
+        <div className='content'>
+          <h1>{ title }</h1>
+          <p>Liked { likes } times</p>
+          {/* <p>{person}</p> */}
+
+          {/* Any variable besides objects are converted to string before being displayed on the DOM */}
+          <p> {10} </p>
+          <p> {'String'} </p>
+          <p> {[1,2,3,4,5]} </p>
+
+          <a href={link}>Click Me</a>
+        </div>
     </div>
   )
 
