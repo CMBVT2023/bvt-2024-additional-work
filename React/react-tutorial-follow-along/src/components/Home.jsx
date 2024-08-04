8b59bf69b4d4582453f4767a09703799e68298a3
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-08-04 09:08:23 -0700

    Finished Video #8 of the react tutorial.

--- React/react-tutorial-follow-along/src/components/Home.jsx
+++ React/react-tutorial-follow-along/src/components/Home.jsx
@@ -1,22 +1,28 @@
+import React, { useState, useEffect } from 'react';
+
+
 const Home = () => {
+    // // This does not trigger a rerender to occur, the variable needs to be reactive for this to work, and this is done with a hook.
+    // let name = 'Mario'
+    // const handleClick = () => {
+    //     name = "Luigi"
+    //     console.log(name)
+    // };
 
-    const handleClick = (e) => {
-        console.log('Hello World', e)
-    };
+    const [Name, setName] = useState('Mario');
+    const [Age, setAge] = useState(25);
 
-    const handleClickAgain = (event, string) => {
-        console.log('Hello World, and ' + string + '.')
-        console.log(event.target)
-    };
+    const handleClick = () => {
+        setName('Luigi')
+        setAge(30);
+    }
  
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            {/* When invoking a function with no parameters */}
-            <button onClick={handleClick}>Click Me</button>
 
-            {/* When invoking a function with parameters */}
-            <button onClick={(e) => handleClickAgain(e, 'Yoshi')}>Click Me Again</button>
+            <p>{Name} is {Age} old.</p>
+            <button onClick={handleClick}>Click Me</button>
         </div>
      );
 }
