f6a7514020c18204df108b67909b62d6697411e2
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-08-04 08:41:47 -0700

    Finished Video #5 of the react tutorial.

--- React/react-tutorial-follow-along/src/App.jsx
+++ React/react-tutorial-follow-along/src/App.jsx
@@ -1,24 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  // const person = {name: 'yoshi', age: 30}
-  const link = 'https://www.google.com'
-
   return (
     <div className="App">
+        <NavBar />
         <div className='content'>
-          <h1>{ title }</h1>
-          <p>Liked { likes } times</p>
-          {/* <p>{person}</p> */}
-
-          {/* Any variable besides objects are converted to string before being displayed on the DOM */}
-          <p> {10} </p>
-          <p> {'String'} </p>
-          <p> {[1,2,3,4,5]} </p>
-
-          <a href={link}>Click Me</a>
+          <Home />
         </div>
     </div>
   )
