6d96a53f723985ed513b9259d67626861cccc459
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-08-05 07:51:38 -0700

    Finished Part 1 of the JSON-Server tutorial

--- Miscellaneous/json-server-tutorial/js/details.js
+++ Miscellaneous/json-server-tutorial/js/details.js
@@ -1,1 +1,22 @@
 // javascript for details.html
+
+// Starting from the question mark to the end of the search bar's string, this is how you can implement specific details being loaded onto a page via details from another html page,
+// in this case it is passing the id of the blog post to load but this could be other things like what value to display or what score a user earned or item they are looking at.
+const id = new URLSearchParams(window.location.search).get('id');
+const Name = new URLSearchParams(window.location.search).get('name');
+const container = document.querySelector('.details');
+
+const renderDetails = async () => {
+    // This specifies to the json server, what post we specifically want to fetch, in this case it will return the post with the id value equal to the id parameter in the url.
+    const res = await fetch('http://localhost:8000/posts/' + id)
+    const post = await res.json();
+
+    const template = `
+        <h1>${post.title}</h1>
+        <p>${post.body}</p>
+    `
+
+    container.innerHTML = template
+}
+
+window.addEventListener('DOMContentLoaded', () => renderDetails())
--- Miscellaneous/json-server-tutorial/js/index.js
+++ Miscellaneous/json-server-tutorial/js/index.js
@@ -1,22 +1,49 @@
-// javascript for index.html
-async function renderPosts() {
-    try {
-        let initialResponse = await (await fetch('http://localhost:8000/posts',))
-        let posts = await initialResponse.json()
-        const blogs = document.getElementsByClassName('blogs');
-        for (const post of posts) {
-            blogs[0].innerHTML += `<div>
-                               <h2>${post.title}</h2>
-                               <p>${post.body}</p>
-                               <button>${post.likes}</button>
-                        </div>`
-        }
-    } catch (error) {
-        console.error(error)
-    }
+// // This was my method of fetching and retrieving all of the info from the json file, I did this based on memory as review.
+// async function renderPosts() {
+//     try {
+//         let initialResponse = await (await fetch('http://localhost:8000/posts',))
+//         let posts = await initialResponse.json()
+//         const blogs = document.getElementsByClassName('blogs');
+//         for (const post of posts) {
+//             blogs[0].innerHTML += `<div>
+//                                <h2>${post.title}</h2>
+//                                <p>${post.body}</p>
+//                                <button>${post.likes}</button>
+//                         </div>`
+//         }
+//     } catch (error) {
+//         console.error(error)
+//     }
     
+// }
+
+// window.addEventListener('DOMContentLoaded', () => renderPosts())
+
+
+// javascript for index.html
+
+const container = document.querySelector('.blogs')
+
+const renderPosts = async () => {
+    let uri = 'http://localhost:8000/posts';
+
+    const res = await fetch(uri);
+    const posts = await res.json();
+
+    let template = '';
+    posts.forEach((post) => {
+        template += `
+            <div class="post">
+                <h2>${post.title}</h2>
+                <p><small>${post.likes} likes</small></p>
+                <p>${post.body.slice(0, 200)}</p>
+                <a href="./details.html?id=${post.id}&name='Mario'">Read More...</a>
+            </div>
+        `
+    })
+    
+    container.innerHTML = template;
+
 }
 
 window.addEventListener('DOMContentLoaded', () => renderPosts())
-
-
