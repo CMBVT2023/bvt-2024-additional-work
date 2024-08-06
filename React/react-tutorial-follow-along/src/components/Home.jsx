cb5d90099a34250744fc6be49d9e9528e0bb64bf
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-08-06 16:31:26 -0700

    Finished Video #19 of the tutorial.

--- React/react-tutorial-follow-along/src/components/Home.jsx
+++ React/react-tutorial-follow-along/src/components/Home.jsx
@@ -4,6 +4,7 @@ import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [ isPending, setIsPending] = useState(true);
+    const [ error, setError ] = useState(null)
 
       useEffect(() => {
         // Async is not available for useEffect, you would need to create a separate function outside of the 
@@ -11,11 +12,21 @@ const Home = () => {
         
         setTimeout(() => {
           fetch('http://localhost:8000/blogs')
-          .then(res => {return res.json()})
+          .then(res => {
+            if(!res.ok) {
+              throw Error('Could Not Fetch Data For That Resource.')
+            }
+            return res.json()
+          })
           .then(data => {
             setBlogs(data);
             setIsPending(false);
-          });
+            setError(null);
+          })
+          .catch((error) => {
+            setError(error.message);
+            setIsPending(false);
+          })
         }, 1000)
       }, []);
 
@@ -25,6 +36,7 @@ const Home = () => {
         <div className="home">
             {/* This will no longer display until blogs has a value, rather than use the if statement like I did on my online grocery store project.
                 Instead this will not display until the blogs value is true, in the initial load it has a value of null so it is false until a value is received. */}
+            { error && <h1>{error}</h1>}
             {isPending && <h1>Loading...</h1>}
             {blogs && <BlogList blogs={blogs} title='All Blogs' />}
         </div>
