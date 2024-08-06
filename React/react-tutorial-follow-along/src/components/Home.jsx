154341b81348e95d0056141af347ba15aea11f4f
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-08-06 16:18:43 -0700

    Finished Video #17 of the tutorial.

--- React/react-tutorial-follow-along/src/components/Home.jsx
+++ React/react-tutorial-follow-along/src/components/Home.jsx
@@ -2,30 +2,25 @@ import React, { useState, useEffect, useDebugValue } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'Mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Mario', id: 3 }
-      ]);
-
-    const [name, setName] = useState('Mario');
-      
-      const handleDelete = (id) => {
-        const newBlogs = blogs.filter(item => item.id !== id);
-        setBlogs(newBlogs);
-      }
+    const [blogs, setBlogs] = useState(null);
 
       useEffect(() => {
-        // let response = fetch('https://github.com/iamshaunjp/Complete-React-Tutorial/blob/lesson-16/dojo-blog/data/db.json' )
+        // Async is not available for useEffect, you would need to create a separate function outside of the 
+        // in order for it to work, making it more practical to use the .then() and .catch() method.
+        fetch('http://localhost:8000/blogs')
+          .then(res => {return res.json()})
+          .then(data => {
+            setBlogs(data)
+          })
       }, [])
 
       
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title='All Blogs' handleDelete={handleDelete} />
-            <button onClick={() => setName('Luigi')}>Change Name</button>
-            <p>{name}</p>
+            {/* This will no longer display until blogs has a value, rather than use the if statement like I did on my online grocery store project.
+                Instead this will not display until the blogs value is true, in the initial load it has a value of null so it is false until a value is received. */}
+            {blogs && <BlogList blogs={blogs} title='All Blogs' />}
         </div>
      );
 }
